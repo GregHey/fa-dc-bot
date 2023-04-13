992be73893b1bdea1224cd33a6433eae7effb238
Greg <tithing00_amity@icloud.com>
2023-04-13 22:24:38 +0200

    v0.1 refactor + yml data

--- bot.py
+++ bot.py
@@ -5,97 +5,77 @@ import datetime
 from dotenv import load_dotenv
 import os
 from utils import *
+import yaml
 load_dotenv()
 
-# CHANNEL_ID = 1092797560579690516
 BOT_PREFIX = '!'
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 IS_VALID_CHANNEL = False
 
 bot = commands.Bot(command_prefix=BOT_PREFIX, intents=discord.Intents().all())
+if os.path.isfile('./data.yml'):
+    with open('data.yml', 'r') as file:
+        sygnatures = yaml.safe_load(file)
+else:
+    sygnatures = {
+        1058415477526896782: {'name': 'Fitz&Adams', 
+                            1058421816353366016: {'name': 'east', 'ids': {'sk': 100, 'sc': 100, 'sr': 100, 'rf': 100}},
+                            1069793385591877672: {'name': 'west', 'ids': {'sk': 10, 'sc': 10, 'sr': 10, 'rf': 10}}
+                            },
+        1089481656605347901: {'name': 'Hitmans',
+                            1093165214725320744: {'name': 'FA', 'ids': {'sk': 0, 'sc': 0, 'sr': 0, 'rf': 0}
+                            }
+                            },
+        1061423234559508531: {'name': 'Gregs',
+                            1096051901117771847: {'name': 'fa-bot', 'ids': {'sk': 0, 'sc': 0, 'sr': 0, 'rf': 0}
+                            }
+                            }
+    }
 
 @bot.event
 async def on_ready():
+        with open('data.yml', 'w') as f:
+            yaml.dump(sygnatures, f, default_flow_style=False)
         print("Hi, please use /!setup/ to use the bot")
-
-eskid = escid = esrid = erfid = wskid = wscid = wsrid = wrfid = 0
 
 year = datetime.date.today().strftime("%Y")
 
-def caseNumber(inCaseType, inRegion='east'):
+def caseNumber(ctx, inCaseType):
     caseType = str.lower(inCaseType)
-    listOfGlobals = globals()
-    id = 0
-    match inRegion:
-        case 'east':
-            match caseType:
-                case 'sk':
-                    listOfGlobals['eskid'] += 1
-                    id = listOfGlobals['eskid']
-                    rslt = f"SK {id}/{year}"
-                case 'sc' :
-                    listOfGlobals['escid'] += 1
-                    id = listOfGlobals['escid']
-                    rslt = f"SC {id}/{year}"
-                case 'sr' :
-                    listOfGlobals['esrid'] += 1
-                    id = listOfGlobals['esrid']
-                    rslt = f"SR {id}/{year}"
-                case 'rf' :
-                    listOfGlobals['erfid'] += 1
-                    id = listOfGlobals['erfid']
-                    rslt = f"RF {id}/{year}"
-                case _  :
-                    rslt = "Nieznany typ sprawy!"
-            return rslt
-        case 'west':
-            match caseType:
-                case 'sk':
-                    listOfGlobals['wskid'] += 1
-                    id = listOfGlobals['wskid']
-                    rslt = f"SK {id}/{year}"
-                case 'sc' :
-                    listOfGlobals['wscid'] += 1
-                    id = listOfGlobals['wscid']
-                    rslt = f"SC {id}/{year}"
-                case 'sr' :
-                    listOfGlobals['wsrid'] += 1
-                    id = listOfGlobals['wsrid']
-                    rslt = f"SR {id}/{year}"
-                case 'rf' :
-                    listOfGlobals['wrfid'] += 1
-                    id = listOfGlobals['wrfid']
-                    rslt = f"RF {id}/{year}"
-                case _  :
-                    rslt = "Nieznany typ sprawy!"
-            return rslt
-
-async def checkAllowedChannel(channelId,region):
-    match region:
-        case "east":
-            if channelId == int(await get_east_id()):
-                rslt = True
-            else:
-                rslt =  False
-        case "west":
-            if channelId == int(await get_west_id()):
-                rslt =  True
-            else:
-                rslt =  False
-        case _:
-            rslt =  False
+    global sygnatures
+    if caseType in sygnatures[ctx.message.guild.id][ctx.channel.id]['ids'].keys():
+        sygnatures[ctx.message.guild.id][ctx.channel.id]['ids'][caseType] += 1
+        rslt = f"SK {sygnatures[ctx.message.guild.id][ctx.channel.id]['ids'][caseType]}/{year}"
+    else:
+        rslt = 0
+    with open('data.yml', 'w') as f:
+        yaml.dump(sygnatures, f, default_flow_style=False)
     return rslt
 
-async def sygn(ctx, caseType, caseName=" ", region="east"):
-    isAllowedChannel = await checkAllowedChannel(ctx.channel.id,region)
+async def checkAllowedChannel(channelId,serverId):
+    global sygnatures
+    if serverId in sygnatures.keys():
+        if channelId in sygnatures[serverId].keys():
+            rslt = True
+        else:
+            rslt = False
+    return rslt
+
+@bot.command()
+async def sygn(ctx, caseType, caseName=" "):
+    isAllowedChannel = await checkAllowedChannel(ctx.channel.id,ctx.message.guild.id)
     if isAllowedChannel:
-        result =  f"{caseNumber(caseType,region)} {caseName}"
-        msg = await ctx.send(f"{result}") 
-        usr = ctx.message.author
-        thread = await msg.create_thread(name=result, auto_archive_duration=10080)
-        await thread.add_user(usr)
+        caseNum = caseNumber(ctx,caseType)
+        if caseNum != 0:
+            result =  f"{caseNum} {caseName}"
+            msg = await ctx.send(f"{result}") 
+            usr = ctx.message.author
+            thread = await msg.create_thread(name=result, auto_archive_duration=10080)
+            await thread.add_user(usr)
+        else:
+            await ctx.send(f"Nieznany typ sprawy [{caseType}]! Wybierz jeden z dostępnych typów: {list(sygnatures[region].keys())}")
     else:
-        await ctx.send(f"{ctx.message.author.mention}, You are not allowed to use this command here.")
+        await ctx.send(f"{ctx.message.author.mention}, Ta komenda nie jest obsługiwana na tym kanale.")
 
 @bot.command()
 async def setup(ctx,param=" ", val=0):
@@ -185,20 +165,13 @@ async def setup(ctx,param=" ", val=0):
         await ctx.send(f"ONLY FOR HITMAN")
 
 @bot.command()
-async def east(ctx, caseType, caseName=""):
-    await ctx.message.delete()
-    await sygn(ctx, caseType, caseName, "east")
-
-@bot.command()
-async def west(ctx, caseType, caseName=""):
-    await ctx.message.delete()
-    await sygn(ctx, caseType, caseName, "west")
-
-@bot.command()
 async def get_id (ctx):
     print (ctx.message.guild.id)
     print (ctx.message.guild)
 
+@bot.command()
+async def info (ctx):
+    print (f"Generator sygnatur dla Fitz&Adams / ALO")
 
 bot.run(BOT_TOKEN)
 
